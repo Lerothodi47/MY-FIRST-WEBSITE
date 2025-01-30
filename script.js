035919352b0ef4aa14a47053ad674eb6f84881c8
Lerothodi47 <163782694+Lerothodi47@users.noreply.github.com>
2025-01-30 15:29:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function hamburg(){
-    const navbar = document.querySelector("dropdown")
+    const navbar = document.querySelector(".dropdown")
     navbar.style.transform = "translateY(0px)"
 }
 
