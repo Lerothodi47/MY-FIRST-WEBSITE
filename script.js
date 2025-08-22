9e76d07399fc366e432b3de9575bf4f8c9aeda6c
Lerothodi47 <163782694+Lerothodi47@users.noreply.github.com>
2025-08-22 20:48:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function hamburg(){
     const navbar = document.querySelector(".dropdown")
+    navbar.classList.add("active");
     navbar.style.transform = "translateY(0px)"
 }
 
 function cancel(){
     const navbar = document.querySelector(".dropdown")
+    navbar.classList.remove("active");
     navbar.style.transform = "translateY(-500px)"
 }
 
